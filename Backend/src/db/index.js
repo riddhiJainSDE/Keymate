717a388be7fc124480b5313b349ecf635ccda09b
riddhiJainSDE <jainriddhi78@gmail.com>
2025-09-13 19:10:07 +0530

    fixing errors

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB=async ()=>{
     try{
         const DB_NAME=process.env.DB_NAME;
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log(`\n MongoDB Connected... DB HOST: ${connectionInstance.connection.host}`);
     } catch(e){
         console.log("MONGODB CONNECTION FAILED",e);
